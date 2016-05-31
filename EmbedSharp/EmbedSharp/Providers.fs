66d98945459d68c246210c0fdc2d650d833cd5c9
Pushparaj Adigopula <pushparaj@adigopula.co.uk>
2016-05-31 23:19:53 +0100

    Provider DTO + GetAllAvailableProviders() now returns Seq<Provider>

--- EmbedSharp/EmbedSharp/Providers.fs
+++ EmbedSharp/EmbedSharp/Providers.fs
@@ -3,14 +3,26 @@
 namespace EmbedSharp
 
 open FSharp.Data
+open System.Linq
+
+//DTO for Json
+type Provider( Name : string, ProviderUrl : string, Schemes : string[], oEmbedUrl : string, IsDiscoverable : Option<bool>, SupportedFormats : string[] ) = 
+    member this.Name = Name
+    member this.ProviderUrl = ProviderUrl
+    member this.Schemes = Schemes
+    member this.OEmbedUrl with get() = oEmbedUrl
+    member this.IsDiscoverable = IsDiscoverable
+    member this.SupportedFormats = SupportedFormats
+
+type oEmbedProviders = JsonProvider<"http://oembed.com/providers.json">
 
 type Providers() = 
-    //Private function to load available providers dynamically
-//    member GetAllAvailableProviders() = 
-//        type oEmbedProviders = JsonProvider<"http://oembed.com/providers.json">
-//        let providerUrls = oEmbedProviders.GetSamples()
-//        providerUrls.
-
-
-    member this.AvailableProvidersCount = 0
-    //member this.
+    member this.GetAllAvailableProviders() = 
+        let providerUrls = oEmbedProviders.Load("http://oembed.com/providers.json")
+        providerUrls 
+            |> Seq.map (fun x -> new Provider (Name = x.ProviderName, 
+                                               ProviderUrl = x.ProviderUrl, 
+                                               Schemes = x.Endpoints.FirstOrDefault().Schemes,
+                                               oEmbedUrl = x.Endpoints.FirstOrDefault().Url,
+                                               IsDiscoverable = x.Endpoints.FirstOrDefault().Discovery,
+                                               SupportedFormats = x.Endpoints.FirstOrDefault().Formats) )
