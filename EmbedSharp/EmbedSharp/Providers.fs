1701eecc15cb8eb3c2cf1923b05eb1d4bdf1f352
Pushparaj Adigopula <pushparaj@adigopula.co.uk>
2016-05-31 20:57:11 +0100

    Added project + script to use json provider
    Added project + script to use json provider

---
+++ EmbedSharp/EmbedSharp/Providers.fs
@@ -0,0 +1,16 @@
+﻿
+
+namespace EmbedSharp
+
+open FSharp.Data
+
+type Providers() = 
+    //Private function to load available providers dynamically
+//    member GetAllAvailableProviders() = 
+//        type oEmbedProviders = JsonProvider<"http://oembed.com/providers.json">
+//        let providerUrls = oEmbedProviders.GetSamples()
+//        providerUrls.
+
+
+    member this.AvailableProvidersCount = 0
+    //member this.
---
+++ EmbedSharp/EmbedSharp/Script.fsx
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#r @"C:\Users\pushparaj\Git Projects\EmbedSharp\EmbedSharp\packages\FSharp.Data.2.3.0\lib\net40\FSharp.Data.dll"
+open System
+open System.Linq
+open FSharp.Data
+// Define your library scripting code here
+
+type oEmbedProviders = JsonProvider<"http://oembed.com/providers.json">
+let providerUrls = oEmbedProviders.Load("http://oembed.com/providers.json")
+providerUrls.First().ProviderUrl;;
+providerUrls.First().Endpoints.First();;
+providerUrls.First().JsonValue;;
+providerUrls.First().ProviderName;;
